23e3d3853226d733c3aaf0aeb6f1e7e87b2e98fd
Adhish Gautam <osiris@kali.kali>
2026-01-19 21:46:08 +0545

    Updating Paywall via JS

---
+++ Code/paybutton.js
@@ -0,0 +1,6 @@
+const buyButton = document.getElementById('buyAlert');
+const displayAlert = document.getElementById('displayAlert');
+
+buyButton.addEventListener('click', function(){
+    displayAlert.style.display = 'flex';
+})
